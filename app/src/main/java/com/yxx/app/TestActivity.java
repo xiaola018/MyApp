1b24561bd9fca309b5f51b1d1fd70ffc30e6f30c
yangla <xiaola018@163.com>
2021-07-02 15:07:27 +0800

    test commit111

--- app/src/main/java/com/yxx/app/TestActivity.java
+++ app/src/main/java/com/yxx/app/TestActivity.java
@@ -1,9 +1,19 @@
 package com.yxx.app;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
 /**
  * Author: yangxl
  * Date: 2021/7/2 14:37
  * Description:
  */
-public class TestActivity {
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
